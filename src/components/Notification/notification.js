a412028ec94ac04c90a24e46af9e0d28d22b63b0
vj2573 <viditi.aks@gmail.com>
2025-04-18 21:43:05 +0530

    "notification front"

--- src/components/Notification/notification.js
+++ src/components/Notification/notification.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import moment from "moment";
-import "./stylesnotification.css"; // Ensure it matches LinkedIn style
+import "./stylesnotification.css"; 
 
 const Notifications = () => {
     const [notifications, setNotifications] = useState([]);
